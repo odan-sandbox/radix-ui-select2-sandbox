23f0d3e057bd5bb845b09b815534f73000e85c40
odan <odan3240@gmail.com>
2023-09-26 01:15:28 +0900

    add radix-ui and storybook

---
+++ src/components/Select2/Select2.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Select2 } from "./Select2";
+
+const meta = {
+  title: "Select2",
+  component: Select2,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof Select2>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {},
+};
